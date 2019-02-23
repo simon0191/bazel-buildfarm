296cddb4b3f19d895f1c54e4a6e394eae9020122
Simon Soriano <simonsp@uber.com>
2019-02-23 17:36:33 +0100

    Workaround to create a bundle jar.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
-# Generates zip files ready to be uploaded to the artifactory
-# set -ex
+# Releases and uploads bundle to artifactory
+# Required: 
+#   - ARTIFACTORY_KEY: Artifactory API Key. To obtain one, go to artifactory-sjc1.uberinternal.com/artifactory/webapp/#/profile
+# Optional:
+#   - BBF_VERSION: Version of the artifact. Defaults to LOCALSNAPSHOT-${CURR_DATE}
+#   - ARTIFACTORY_URL: Defaults to http://artifactory.uber.internal:4587
+#   - ARTIFACTORY_REPO: Defaults to artifactory/libs-release-local
 
-# bazel build farm version
-readonly BBF_VERSION="${1:-"LOCAL-SNAPSHOT"}"
+set -ex
 readonly REPO_ROOT="$(git rev-parse --show-toplevel)"
+pushd $REPO_ROOT
+trap popd EXIT
+
+readonly CURR_DATE=$(date +'%Y%m%d%H%M%S')
+readonly BBF_VERSION=${1:-"LOCALSNAPSHOT-${CURR_DATE}"}
 readonly OUT_DIR="${REPO_ROOT}/tmp"
+readonly ARTIFACTORY_URL=${ARTIFACTORY_URL:-"http://artifactory.uber.internal:4587"}
+readonly ARTIFACTORY_REPO=${ARTIFACTORY_REPO:-"artifactory/libs-release-local"}
+
+upload_to_artifactory() {
+  local LOCAL_PATH="${1}"
+  local ARTIFACT_PATH="${2}"
+
+  curl -H "X-JFrog-Art-Api:${ARTIFACTORY_KEY}" \
+      -T $LOCAL_PATH \
+      -X PUT "${ARTIFACTORY_URL}/${ARTIFACTORY_REPO}/${ARTIFACT_PATH}"
+}
 
 release() {
   local VERSION_NAME="$1"
   local TARGET_NAME="$2"
-  local BAZEL_BIN_PATH="./bazel-bin/src/main/java/build/buildfarm"
 
-  local TARGET_OUT_DIR="${OUT_DIR}/${TARGET_NAME}-${VERSION_NAME}"
-  local ARTIFACT_PATH="${TARGET_OUT_DIR}/build/buildfarm/${TARGET_NAME}/${VERSION_NAME}"
+  local BAZEL_BIN_PATH="${REPO_ROOT}/bazel-bin/src/main/java/build/buildfarm"
+  local ARTIFACT_PATH="build/buildfarm/${TARGET_NAME}/${VERSION_NAME}"
 
-  # Build target and pom file
   ./bazelw build --define=pom_version=${VERSION_NAME} \
     //src/main/java/build/buildfarm:${TARGET_NAME} \
     //src/main/java/build/buildfarm:${TARGET_NAME}_pom
 
-  # Clean output directory
-  rm -rf $TARGET_OUT_DIR
-  mkdir -p $ARTIFACT_PATH
-
-  # Move pom, jar and sources files to output dir
-  cp "${BAZEL_BIN_PATH}/lib${TARGET_NAME}.jar" "$ARTIFACT_PATH/${TARGET_NAME}-${VERSION_NAME}.jar"
-  cp "${BAZEL_BIN_PATH}/${TARGET_NAME}_pom.xml" "$ARTIFACT_PATH/${TARGET_NAME}-${VERSION_NAME}.pom"
-  cp "${BAZEL_BIN_PATH}/lib${TARGET_NAME}-src.jar" "$ARTIFACT_PATH/${TARGET_NAME}-${VERSION_NAME}-sources.jar"
-
-  # Build zip
-  pushd ${TARGET_OUT_DIR}
-  zip -r build.zip build
-  popd
-
-  echo "----> zip: ${TARGET_OUT_DIR}/build.zip"
+  upload_to_artifactory "${BAZEL_BIN_PATH}/lib${TARGET_NAME}.jar" "$ARTIFACT_PATH/${TARGET_NAME}-${VERSION_NAME}.jar"
+  upload_to_artifactory "${BAZEL_BIN_PATH}/${TARGET_NAME}_pom.xml" "$ARTIFACT_PATH/${TARGET_NAME}-${VERSION_NAME}.pom"
 }
 
-release $BBF_VERSION "ac"
-release $BBF_VERSION "cas"
-release $BBF_VERSION "common"
-release $BBF_VERSION "http-proxy"
-release $BBF_VERSION "instance"
-release $BBF_VERSION "memory-instance"
-release $BBF_VERSION "operationqueue-worker"
-release $BBF_VERSION "server-instance"
-release $BBF_VERSION "server"
-release $BBF_VERSION "stub-instance"
-release $BBF_VERSION "worker"
+release $BBF_VERSION "bundle"
---
+++ src/main/java/build/buildfarm/Ignore.java
@@ -0,0 +1,4 @@
+package build.buildfarm;
+
+public class Ignore {
+}
